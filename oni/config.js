f6b5e6133c05c5b622da0eb9f371114568ffca0f
Cameron Diver <cameron@resin.io>
2018-04-02 09:55:02 +0100

    more stuff

---
+++ oni/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	"oni.useDefaultConfig":false,
+     "oni.loadInitVim": true,
+  //add custom config here, such as
+	// "oni.useDefaultConfig": true,
+	"oni.hideMenu": true,
+	"editor.fontSize": "12px",
+	// "editor.fontFamily": "Monaco",
+	"editor.fontFamily": "Source code pro",
+	// "tabs.enabled": true,
+	// "tabs.showVimTabs": true,
+    "ui.colorscheme": "vim-material",
+    "vim.setting.list": false,
+	"oni.bookmarks": [
+		"/home/cameron/Resin/devenv/containers/src/builder/",
+		"/home/cameron/Resin/modules/resin-bundle-resolve"
+	]
+};
